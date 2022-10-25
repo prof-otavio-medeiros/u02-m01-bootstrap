9def06513270d02bcadd1e6e58626131fe4975ae
Prof. Otávio Medeiros <prof.otavio.medeiros@gmail.com>
2022-10-24 22:50:34 -0300

    main > commit 002 > 2022-10-24 22:50:34 -03:00:00

--- scripts/01-container.js
+++
@@ -1,8 +0,0 @@
-/*
-    Not working. See:
-        https://stackoverflow.com/questions/6492683/how-to-detect-divs-dimension-changed
-*/
-function getSize(element) {
-    let text = element.innerText;
-    text.replace("--", element.offsetWidth);
-}
